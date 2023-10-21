5207ba93ab0cb9a40256cdaa08a20dcf33ed3151
Haruki <underbird.h@gmail.com>
2023-10-21 19:14:54 +0900

    ぽいうごきはした

--- sensor_test.m
+++ sensor_test.m
@@ -3,12 +3,16 @@ syms theta
 theta_l = 5;
 max = 1000;
 
-% eq = -(4 * max / theta_l^2)*theta * (theta - theta_l);
 eq_l = - (max / (2 * (theta_l^2))) * (theta - theta_l)^2 + max;
 eq_r = - (max / (2 * (theta_l^2))) * (theta + theta_l)^2 + max;
 
+figure(1)
 fplot(eq_l, [-2*theta_l, 2*theta_l])
 hold on
 fplot(eq_r, [-2*theta_l, 2*theta_l])
 hold off
 ylim([0, max])
+
+figure(2)
+eq_diff = eq_r - eq_l; % 差を取ると結局線形になる
+fplot(eq_diff)
